452ac2a5da6bfff873c686b210cddeb0315125a8
Jindřich Moravec <xmorave2@fi.muni.cz>
2023-11-24 00:02:51 +0100

    test: add tests for bvh building and collision

--- collisioner/src/collisions/bvh.rs
+++ collisioner/src/collisions/bvh.rs
@@ -133,6 +133,14 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
 
+    fn cbox(x: f64, y: f64, z: f64, w: f64, h: f64, d: f64) -> Collider {
+        Collider::aligned_box(x, y, z, w, h, d)
+    }
+
+    fn cpoint(x: f64, y: f64, z: f64) -> Collider {
+        Collider::point(x, y, z)
+    }
+
     #[test]
     fn build_bhv_correct() {
         let objects = vec![
@@ -213,11 +221,96 @@ mod tests {
         );
     }
 
-    fn cbox(x: f64, y: f64, z: f64, w: f64, h: f64, d: f64) -> Collider {
-        Collider::aligned_box(x, y, z, w, h, d)
+    #[test]
+    fn longest_axis() {
+        let objects = vec![
+            cpoint(1.0, 1.0, 1.0),
+            cpoint(2.0, 2.0, 2.0),
+            cpoint(3.0, 3.0, 3.0),
+            cpoint(4.0, 4.0, 4.0),
+        ];
+
+        assert_eq!(Axis::X, BvhTree::longest_axis(&objects[..]));
     }
 
-    fn cpoint(x: f64, y: f64, z: f64) -> Collider {
-        Collider::point(x, y, z)
+    #[test]
+    fn order_by_axis() {
+        let objects = vec![
+            cpoint(1.0, 1.0, 1.0),
+            cpoint(2.0, 2.0, 2.0),
+            cpoint(3.0, 3.0, 3.0),
+            cpoint(4.0, 4.0, 4.0),
+        ];
+
+        let ordered = BvhTree::order_by_axis(&objects[..], Axis::X);
+
+        assert_eq!(objects[0], ordered[0]);
+        assert_eq!(objects[1], ordered[1]);
+        assert_eq!(objects[2], ordered[2]);
+        assert_eq!(objects[3], ordered[3]);
+    }
+
+    #[test]
+    fn collider_to_point() {
+        let c = cbox(1.0, 1.0, 1.0, 2.0, 2.0, 2.0);
+        let p = BvhTree::collider_to_point(&c);
+
+        assert_eq!(Vector3::new(1.0, 1.0, 1.0), p);
+    }
+
+    #[test]
+    fn colliders_to_bounding() {
+        let colliders = vec![
+            cbox(1.0, 1.0, 1.0, 2.0, 2.0, 2.0),
+            cbox(2.0, 2.0, 2.0, 3.0, 3.0, 3.0),
+            cbox(3.0, 3.0, 3.0, 4.0, 4.0, 4.0),
+            cbox(4.0, 4.0, 4.0, 5.0, 5.0, 5.0),
+        ];
+
+        let bounding = BvhTree::colliders_to_bounding(&colliders[..]);
+
+        assert_eq!(cbox(3.25, 3.25, 3.25, 6.5, 6.5, 6.5), bounding);
+    }
+
+    #[test]
+    fn nodes_collide_some() {
+        let a = Some(Box::new(BvhTree::Leaf(cpoint(1.0, 1.0, 1.0))));
+        let b = Some(Box::new(BvhTree::Leaf(cpoint(1.0, 1.0, 1.0))));
+
+        assert!(BvhTree::nodes_collide(&a, &b));
+    }
+
+    #[test]
+    fn nodes_collide_some_none() {
+        let a = Some(Box::new(BvhTree::Leaf(cpoint(1.0, 1.0, 1.0))));
+        let b = None;
+
+        assert!(!BvhTree::nodes_collide(&a, &b));
+    }
+
+    #[test]
+    fn leafs_collide() {
+        let a = BvhTree::Leaf(cpoint(1.0, 1.0, 1.0));
+        let b = BvhTree::Leaf(cpoint(1.0, 1.0, 1.0));
+
+        assert!(a.collides_with(&b));
+    }
+
+    #[test]
+    fn branches_collide() {
+        let leaf_a = BvhTree::Leaf(cpoint(1.0, 1.0, 1.0));
+        let branch_a = BvhTree::Branch(
+            cbox(1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+            None,
+            Some(Box::new(leaf_a)),
+        );
+        let leaf_b = BvhTree::Leaf(cpoint(1.0, 1.0, 1.0));
+        let branch_b = BvhTree::Branch(
+            cbox(1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+            Some(Box::new(leaf_b)),
+            None,
+        );
+
+        assert!(branch_a.collides_with(&branch_b));
     }
 }
