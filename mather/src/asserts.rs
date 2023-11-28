dc6c5cd98777eaa4ecfd3b51cfad8600e6d76cc6
Jindřich Moravec <xmorave2@fi.muni.cz>
2023-11-29 00:41:27 +0100

    docs: add documentation to asserts

--- mather/src/asserts.rs
+++ mather/src/asserts.rs
@@ -1,9 +1,24 @@
+//! # Math Asserts
+//! Asserts for `Vector3` and `Quaternion` useful for testing.
+//!
+//! ## Example
+//! ```
+//! use mather::asserts::*;
+//! use mather::Vector3;
+//!
+//! let expected = Vector3::new(1.0, 2.0, 3.0);
+//! let actual = Vector3::new(1.0, 2.0, 3.0);
+//! assert_vectors(expected, actual);
+//! ```
+
 use crate::{Quaternion, Vector3};
 
+/// Compares vectors with epsilon of 1e-6.
 pub fn assert_vectors(expected: Vector3, actual: Vector3) {
     assert_vectors_close(expected, actual, 1e-6);
 }
 
+/// Compares vectors with given epsilon.
 pub fn assert_vectors_close(expected: Vector3, actual: Vector3, eps: f64) {
     assert!(
         (expected.x() - actual.x()).abs() < eps,
@@ -25,10 +40,12 @@ pub fn assert_vectors_close(expected: Vector3, actual: Vector3, eps: f64) {
     );
 }
 
+/// Compares quaternions with epsilon of 1e-6.
 pub fn assert_quaternion(expected: Quaternion, actual: Quaternion) {
     assert_quaternion_close(expected, actual, 1e-6);
 }
 
+/// Compares quaternions with given epsilon.
 pub fn assert_quaternion_close(expected: Quaternion, actual: Quaternion, eps: f64) {
     assert!(
         (expected.x() - actual.x()).abs() < eps,
